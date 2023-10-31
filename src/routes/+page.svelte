3ed8a1bdc91adf813267a8f09c6c4b57e9015074
Maxwell Muhanda <ajaybullec@gmail.com>
2023-10-31 23:34:09 +0300

    inital commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+let title = "Ninja gaming guides"
+
+const updateTitle = ()=>{
+  title = "something else"
+}
+</script>
+
+<div class="index">
+  <h2>{title}</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ullam earum deserunt ipsum provident reiciendis, dolore fugiat sunt molestiae totam esse ipsa ea magnam excepturi nulla saepe id quis perferendis nobis recusandae quos assumenda error eligendi voluptatem? Dolorem, doloribus maxime optio libero provident temporibus cupiditate hic. Odit repudiandae vitae architecto!</p>
+  <button on:click={updateTitle}>change title</button>
+  <input type="text" bind:value={title} >
+</div>
+
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,11 @@
+<div class="about">
+    <h2>About us</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam commodi quas, ad suscipit natus animi nesciunt, quidem, impedit eos cupiditate dicta? Odio cum dignissimos ratione excepturi vitae distinctio voluptatum magni.</p>
+  </div>
+  
+  <style>
+    .about
+    {text-align: center;
+      display: block;
+      margin: 20px auto;}
+  </style>
